70f0d9ad44657c1061df51be090387662175b23f
Kinsey Ann <kbooboo@kinseys-MacBook-Pro-2.local>
2014-04-15 15:51:41 -0600

    Add homepage and registration link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'home#index'
 
   resources :users, only: [:new, :create]
 end
--- spec/features/user_can_register_spec.rb
+++ spec/features/user_can_register_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 feature 'User can register' do
   scenario 'with a valid email and password' do
     visit root_path
+    click_on 'Register'
 
     fill_in 'Email', with: 'kinsey@example.com'
     fill_in 'Password', with: 'password'
